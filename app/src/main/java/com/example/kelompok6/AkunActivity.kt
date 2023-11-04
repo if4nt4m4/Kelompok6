450a349cbe937bdd0d50338f8fd8437391548b0e
if4nt4m4 <kresnabayu164@gmail.com>
2023-11-04 08:00:32 +0700

    menambahkan kode pada halaman akun

---
+++ app/src/main/java/com/example/kelompok6/AkunActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kelompok6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AkunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_akun)
+    }
+}
