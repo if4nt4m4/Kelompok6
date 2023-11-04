c82335019f7a91f0cdc155f248dbe7d906450be7
if4nt4m4 <kresnabayu164@gmail.com>
2023-11-04 10:46:58 +0700

    menambahkan kode pada halaman home dan kota

--- app/src/main/java/com/example/kelompok6/MainActivity.kt
+++ app/src/main/java/com/example/kelompok6/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
