eaec52bb4723b0448684ecfcf02a64cce7fc88e1
if4nt4m4 <kresnabayu164@gmail.com>
2023-11-04 08:41:43 +0700

    menambahkan kode pada halaman akun

--- app/src/main/java/com/example/kelompok6/MainActivity.kt
+++ app/src/main/java/com/example/kelompok6/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.kelompok6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val akunImageView = findViewById<ImageView>(R.id.iv_akun)
+
+        akunImageView.setOnClickListener {
+            val intent = Intent(this, AkunActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
