36aec52e58f1f2ac870fc8149b183484fdbe0552
Fariz <farizbakur@gmail.com>
2023-11-05 20:08:35 +0700

    Menambahkan TicketActivity dan memperbaiki MainActivity

--- app/src/main/java/com/example/kelompok6/MainActivity.kt
+++ app/src/main/java/com/example/kelompok6/MainActivity.kt
@@ -20,30 +20,5 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AkunActivity::class.java)
             startActivity(intent)
         }
-
-        setContentView(R.layout.activity_hotel_ticket_list)
-        val recyclerView = findViewById<RecyclerView>(R.id.rv_hotelticket)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        // Membuat adapter dan menghubungkannya dengan recyclerView
-        val ticketList = createTicketList()
-        val adapter = TicketAdapter(ticketList)
-        recyclerView.adapter = adapter
-    }
-
-    private fun createTicketList(): MutableList<Ticket> {
-        val ticketList = mutableListOf<Ticket>()
-
-        // Contoh tiket pertama
-        val ticket1 = Ticket("Hotel ABC", "2023-11-15", "Double Room", "2023-12-15")
-        ticketList.add(ticket1)
-
-        // Contoh tiket kedua
-        val ticket2 = Ticket("Hotel XYZ", "2023-11-20", "Suite Room", "2023-12-20")
-        ticketList.add(ticket2)
-
-        // Anda dapat terus menambahkan tiket sesuai kebutuhan
-
-        return ticketList
     }
 }
