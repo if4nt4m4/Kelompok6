54f55ef14f633af131d08f7a8983d65e1c471c57
if4nt4m4 <kresnabayu164@gmail.com>
2023-11-05 13:56:31 +0700

    menambahkan halaman dan kode home admin dan kist hotel

--- app/src/main/java/com/example/kelompok6/HomeAdminActivity.kt
+++ app/src/main/java/com/example/kelompok6/HomeAdminActivity.kt
@@ -1,11 +1,27 @@
 package com.example.kelompok6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class HomeAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_admin)
+
+        val backImageView = findViewById<ImageView>(R.id.ic_back)
+        val manageButton = findViewById<Button>(R.id.bt_manage)
+
+        backImageView.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        manageButton.setOnClickListener{
+            val intent = Intent(this, ListHotelActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/kelompok6/ListHotelActivity.kt
+++ app/src/main/java/com/example/kelompok6/ListHotelActivity.kt
@@ -1,11 +1,20 @@
 package com.example.kelompok6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ListHotelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_hotel)
+
+        val backImageView = findViewById<ImageView>(R.id.ic_back)
+
+        backImageView.setOnClickListener {
+            val intent = Intent(this, HomeAdminActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
