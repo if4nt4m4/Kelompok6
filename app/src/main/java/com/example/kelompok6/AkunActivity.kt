23ac608f74039d13cf4ffe626cdc693b32058ca0
Fariz <farizbakur@gmail.com>
2023-11-23 19:01:55 +0700

    Menambahkan Fungsi Batal pada AkunActivity

--- app/src/main/java/com/example/kelompok6/AkunActivity.kt
+++ app/src/main/java/com/example/kelompok6/AkunActivity.kt
@@ -1,11 +1,21 @@
 package com.example.kelompok6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class AkunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_akun)
+
+        val btnBatal = findViewById<Button>(R.id.bt_batal)
+
+        btnBatal.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
