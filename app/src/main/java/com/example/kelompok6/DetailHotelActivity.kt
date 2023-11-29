9fe6b4f3ddd743c25f9a2b5c231d641bf1b69564
if4nt4m4 <kresnabayu164@gmail.com>
2023-11-29 10:49:57 +0700

    mau nambahin kode activity tapi masih malas

--- app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
+++ app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
@@ -2,10 +2,21 @@ package com.example.kelompok6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kelompok6.databinding.ActivityDetailHotelBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class DetailHotelActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+    lateinit var binding: ActivityDetailHotelBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_hotel)
+
+        database = FirebaseDatabase.getInstance().reference
+
+        binding.btnPesanSekarang.setOnClickListener {
+            tambahData()
+        }
     }
 }
--- app/src/main/java/com/example/kelompok6/DetailPembayaranActivity.kt
+++ app/src/main/java/com/example/kelompok6/DetailPembayaranActivity.kt
@@ -2,8 +2,11 @@ package com.example.kelompok6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kelompok6.databinding.ActivityDetailPembayaranBinding
 
 class DetailPembayaranActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailPembayaranBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_pembayaran)
