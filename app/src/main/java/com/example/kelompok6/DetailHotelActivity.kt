4f18377a289cc0aa5d0cd52f2d69f9126e342ee1
if4nt4m4 <kresnabayu164@gmail.com>
2023-12-03 09:42:15 +0700

    Menambahkan kode pada detail Pembayaran activity dan detail hotel activity

--- app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
+++ app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
@@ -1,7 +1,13 @@
 package com.example.kelompok6
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 import com.example.kelompok6.databinding.ActivityDetailHotelBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -9,14 +15,52 @@ import com.google.firebase.database.FirebaseDatabase
 class DetailHotelActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
     lateinit var binding: ActivityDetailHotelBinding
+
+    private lateinit var PesanSekarang: Button
+    private lateinit var kamarDeluxe: RadioButton
+    private lateinit var kamarSuperior: RadioButton
+    private lateinit var kamarStandard: RadioButton
+    private lateinit var jumlahKamar: EditText
+    private lateinit var totalPembayaran: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_hotel)
 
+        PesanSekarang = findViewById(R.id.btn_pesan_sekarang)
+        kamarDeluxe = findViewById(R.id.rb_deluxe)
+        kamarSuperior = findViewById(R.id.rb_superior)
+        kamarStandard = findViewById(R.id.rb_standard)
+        jumlahKamar = findViewById(R.id.et_jmlh_kamar)
+        totalPembayaran = findViewById(R.id.tv_total_pembayaran)
+
         database = FirebaseDatabase.getInstance().reference
 
         binding.btnPesanSekarang.setOnClickListener {
-            tambahData()
+            val jumlahKamar = jumlahKamar.text.toString().toIntOrNull()
+
+            if (jumlahKamar == null){
+                Toast.makeText(this, "Jumlah kamar harus berupa bilangan bulat", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            var  hargaKamar = 0
+            if (kamarDeluxe.isChecked){
+                hargaKamar = 500000
+            }else if(kamarSuperior.isChecked){
+                hargaKamar = 400000
+            }else if (kamarStandard.isChecked){
+                hargaKamar = 300000
+            }
+
+            var totalPembayaran = jumlahKamar * hargaKamar
+
+            if (jumlahKamar <= 0){
+                Toast.makeText(this, "Jumlah kamar harus diatas 0", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            this.totalPembayaran.text = "$totalPembayaran"
         }
     }
 }
--- app/src/main/java/com/example/kelompok6/DetailPembayaranActivity.kt
+++ app/src/main/java/com/example/kelompok6/DetailPembayaranActivity.kt
@@ -3,12 +3,32 @@ package com.example.kelompok6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kelompok6.databinding.ActivityDetailPembayaranBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class DetailPembayaranActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityDetailPembayaranBinding
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var binding: ActivityDetailPembayaranBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_pembayaran)
+
+        databaseReference = FirebaseDatabase.getInstance().reference
+
+        binding.btnCetakPesan.setOnClickListener {
+            val pemesanan = hashMapOf(
+                "namaHotel" to binding.tvNmhotel.text.toString(),
+                "alamat" to binding.tvAlamatHotel.text.toString(),
+                "tipekamar" to binding.tvTipekamar.text.toString(),
+                "jumlahPembayaran" to binding.tvJmlhpembayaran.text.toString(),
+                "tanggalCheckIn" to binding.etTglpesan.text.toString(),
+                "tanggalCheckOut" to binding.etTglkeluar.text.toString(),
+                "namaPemesan" to binding.etNamapemesan.text.toString(),
+                "noHp" to binding.etNohpUser.text.toString(),
+                "email" to binding.etEmailUser.text.toString(),
+                "umur" to binding.etUmuruser.text.toString()
+            )
+        }
     }
 }
