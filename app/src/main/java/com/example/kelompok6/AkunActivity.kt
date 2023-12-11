e07833bc1590af4b01c44cda92a55ec85de1a81d
Fariz <farizbakur@gmail.com>
2023-12-11 14:28:44 +0700

    adding logout function to AkunActivity

--- app/src/main/java/com/example/kelompok6/AkunActivity.kt
+++ app/src/main/java/com/example/kelompok6/AkunActivity.kt
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import com.google.firebase.auth.FirebaseAuth
 
 class AkunActivity : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_akun)
 
-        val btnBatal = findViewById<Button>(R.id.bt_batal)
+        val btnLogout = findViewById<Button>(R.id.bt_logout)
 
-        btnBatal.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        btnLogout.setOnClickListener {
+            btnLogout()
         }
+    }
+
+    private fun btnLogout(){
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
     }
 }
