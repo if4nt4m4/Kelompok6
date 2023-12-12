98625cff585916f7759eec93ade50789e11f7332
Fariz <farizbakur@gmail.com>
2023-12-12 23:27:44 +0700

    percobaan menampilkan data pada DetailHotelActivity (belum berhasil)

--- app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
+++ app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
@@ -1,10 +1,12 @@
 package com.example.kelompok6
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kelompok6.databinding.ActivityDetailHotelBinding
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.*
 
 class DetailHotelActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
@@ -14,19 +16,43 @@ class DetailHotelActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailHotelBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        binding.ivHotel
-//        binding.tvNamaHotel
-//        binding.etTanggalCheckIn
-//        binding.etTanggalCheckOut
-//        binding.tvTipekamar
-//        binding.rgTipeKamar
-//        binding.etJmlhKamar
+        FirebaseApp.initializeApp(this)
 
         database = FirebaseDatabase.getInstance().reference
 
-        binding.btnPesanSekarang.setOnClickListener {
+        // Query untuk mendapatkan data hotel dengan itemName "ayo fahrul"
+        val query = database.child("Hotel").orderByChild("itemName").equalTo("ayo fahrul")
 
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    // Mendapatkan data hotel dari snapshot
+                    for (hotelSnapshot in snapshot.children) {
+                        val hotel = hotelSnapshot.getValue(Hotel::class.java)
+
+                        // Menampilkan data hotel di antarmuka pengguna
+                        binding.tvNamaHotel.text = hotel?.itemName
+                        binding.tvAlamatHotel.text = hotel?.itemAddress
+                        // ... set data lainnya sesuai kebutuhan
+
+                        // Menampilkan gambar jika ada
+                        if (!hotel?.itemImg.isNullOrEmpty()) {
+                            val decodedImage = android.util.Base64.decode(hotel?.itemImg, android.util.Base64.DEFAULT)
+                            val bitmap = BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.size)
+                            binding.ivHotel.setImageBitmap(bitmap)
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("Database Error: ", error.getMessage())
+            }
+        })
+
+        binding.btnPesanSekarang.setOnClickListener {
+            // Logika untuk menangani tombol pesan sekarang
         }
     }
 }
+
---
+++ app/src/main/java/com/example/kelompok6/Hotel.kt
@@ -0,0 +1,15 @@
+package com.example.kelompok6
+
+data class Hotel(
+    val itemImg: String? = "",
+    val itemName: String? = null,
+    val itemAddress: String? = null,
+    val itemWis: String? = null,
+    val itemFas: String? = null,
+    val itemTK1: String? = null,
+    val itemHTK1: Int? = null,
+    val itemTK2: String? = null,
+    val itemHTK2: Int? = null,
+    val itemTK3: String? = null,
+    val itemHTK3: Int? = null
+)
