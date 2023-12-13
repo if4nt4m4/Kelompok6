891d2c7086f428fe9210d021fc742b45b52fc462
Fariz <farizbakur@gmail.com>
2023-12-13 19:33:48 +0700

    updating DetailHotelActivity (menambahkan fungsi pemilihan tanggal)

--- app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
+++ app/src/main/java/com/example/kelompok6/DetailHotelActivity.kt
@@ -1,16 +1,21 @@
 package com.example.kelompok6
 
+import android.app.DatePickerDialog
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kelompok6.databinding.ActivityDetailHotelBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.*
+import java.util.*
 
 class DetailHotelActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
     private lateinit var binding: ActivityDetailHotelBinding
+    private lateinit var selectedDateTextView: TextView // Add this line
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,9 @@ class DetailHotelActivity : AppCompatActivity() {
         FirebaseApp.initializeApp(this)
 
         database = FirebaseDatabase.getInstance().reference
+
+        // Initialize TextView for selected date
+        selectedDateTextView = binding.tvTanggalCheckIn // Update this line
 
         // Query untuk mendapatkan data hotel dengan itemName "ayo fahrul"
         val query = database.child("Hotel").orderByChild("itemName").equalTo("ayo fahrul")
@@ -50,9 +58,36 @@ class DetailHotelActivity : AppCompatActivity() {
             }
         })
 
+        // Set click listener for date pickers
+        binding.tvTanggalCheckIn.setOnClickListener {
+            showDatePickerDialog(binding.tvTanggalCheckIn)
+        }
+
+        binding.tvTanggalCheckOut.setOnClickListener {
+            showDatePickerDialog(binding.tvTanggalCheckOut)
+        }
+
         binding.btnPesanSekarang.setOnClickListener {
             // Logika untuk menangani tombol pesan sekarang
         }
     }
-}
 
+    private fun showDatePickerDialog(textView: TextView) {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                // Update the TextView with the selected date
+                val selectedDate = "$dayOfMonth/${month + 1}/$year"
+                textView.text = selectedDate
+            },
+            year, month, day
+        )
+
+        datePickerDialog.show()
+    }
+}
