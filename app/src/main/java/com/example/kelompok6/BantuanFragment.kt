bdf962921f0321c72f22d43c4c90a970b67891f2
AnugrahGilang <nakmediun@gmail.com>
2023-12-14 01:36:49 +0700

    Membuat tombol kembali agar dapat kembali ke halaman sebelumnya

--- app/src/main/java/com/example/kelompok6/BantuanFragment.kt
+++ app/src/main/java/com/example/kelompok6/BantuanFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 
 
 class BantuanFragment : Fragment() {
@@ -15,6 +17,14 @@ class BantuanFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bantuan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageView>(R.id.ic_back).setOnClickListener{
+            findNavController().navigateUp()
+        }
     }
 
 }
